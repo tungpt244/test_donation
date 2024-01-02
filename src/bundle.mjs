285e8fcd64b05cf7d1bad5f616e89881da392a77
Pham Thanh Tung <tungpt244@gmail.com>
2024-01-02 15:18:38 +0700

    docker

--- src/bundle.mjs
+++ src/bundle.mjs
@@ -8,6 +8,8 @@ export const useDonation = async ({ _site, _name, _message, _loop }) => {
     .launch({
       headless: true,
       args: ['--disable-setuid-sandbox', '--no-sandbox', '--single-process', '--no-zygote'],
+      executablePath:
+        process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
     })
     .catch(error => {
       console.log(error)
